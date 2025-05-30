a88c1de5a2aaf1feda88724874489e3a0c30521b
Hpastel <hermesalvesporto@gmail.com>
2025-05-30 12:09:27 +0100

    Herois

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-let heroi = "";
+let heroi = ["Superman", "Batman", "Mulher Maravilha", "Flash", "Aquaman", "Lanterna Verde", "Ciborgue", "Arqueiro Verde"];
 const nivel = [1000, 1001, 2001, 5001, 7001, 8001, 9001, 10001];
 
 for (let i = 0; i < nivel.length; i++) { 
-    console.log("O Herói de nome " + heroi + " está no nível de " + nivel);
+    console.log("O Herói de nome " + heroi[7] + " está no nível de " + nivel[0]);
+
 }
 
