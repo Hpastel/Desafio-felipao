d0f24efcae2db8e13b70afb6112e76c2aefed052
Hpastel <hermesalvesporto@gmail.com>
2025-05-30 16:32:21 +0100

    atualização codigo

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ let heroi = ["Superman", "Batman", "Mulher Maravilha", "Flash", "Aquaman", "Lant
 const nivel = [1000, 1001, 2001, 5001, 7001, 8001, 9001, 10001];
   
 
-console.log("O Herói de nome " + heroi[7] + " está no nível de " + nivel[0]);
-console.log(heroi.length + " Heróis foram cadastrados com sucesso.");
+// console.log("O Herói de nome " + heroi[7] + " está no nível de " + nivel[0]);
+// console.log(heroi.length + " Heróis foram cadastrados com sucesso.");
 
 let nivelHeroi = false
-let heroiEscolhido = heroi;
+let heroiEscolhido = heroi === "";
 
-if (nivelHeroi === "") {
+if (nivelHeroi === "false") {
     console.log("Escolha um nivel " + nivel[console.nivel] + " e conheça o heroí");
 
 } else (nivelHeroi === null || nivelHeroi === undefined); {
