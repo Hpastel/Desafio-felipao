020a0490958af0901982e3ed5aa249d2920d03de
Hpastel <hermesalvesporto@gmail.com>
2025-05-30 17:45:15 +0100

    experimentando no codigo

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ const nivel = [1000, 1001, 2001, 5001, 7001, 8001, 9001, 10001];
 // console.log("O Herói de nome " + heroi[7] + " está no nível de " + nivel[0]);
 // console.log(heroi.length + " Heróis foram cadastrados com sucesso.");
 
-let nivelHeroi = false
+let nivelHeroi = heroi + nivel;
 let heroiEscolhido = heroi === "";
+
+console.log(nivelHeroi[1]);
+
 
 if (nivelHeroi === "false") {
     console.log("Escolha um nivel " + nivel[console.nivel] + " e conheça o heroí");
@@ -16,5 +19,16 @@ if (nivelHeroi === "false") {
 }
 
 
-// for (let i = 0; i < nivel.length; i++) { 
-// }
+//  for (let contador = 1; contador < 1000; contador++) {
+//      console.log("O herói " + heroi[contador] + " está no nível " + nivel [0]);
+//  }
+
+
+
+
+
+
+
+
+
+
