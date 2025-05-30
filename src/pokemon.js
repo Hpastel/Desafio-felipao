bedec9bb60e32c5741be9b1c33129bc57441cc35
Hpastel <hermesalvesporto@gmail.com>
2025-05-30 12:27:40 +0100

    atualização do codigo pokemon.js

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,31 +1,31 @@
 let pokemon = [
-	["Dragonite", 2, "Sombrio", 7],
-    ["Hurmple", 3, "Fogo", 13],
-    ["Poochyena", 5, "Inseto", 15],
-    ["Zigzagoon", 7, "Normal", 24]
+	["Dragonite", "level 2", "Sombrio", 7],
+    ["Hurmple", " level 3", "Fogo", 13],
+    ["Poochyena", "level 5", "Inseto", 15],
+    ["Zigzagoon", "level 7", "Normal", 24]
 ]
 
 console.log(
 	pokemon[0][0] + " Cadastrado com sucesso. " + 
     " Seu HP após a ultima luta é: " + pokemon [3][3] + 
-    ". Está no nivél " + pokemon[2][1] + 
+    ". Está no " + pokemon[2][1] + 
     ". E é do tipo " + pokemon[1][2] 
 )
 console.log(
 	pokemon[3][0] + " Cadastrado com sucesso. " + 
     " Seu HP após a ultima luta é: " + pokemon [1][3] + 
-    ". Está no nivél " + pokemon[0][1] + 
+    ". Está no " + pokemon[0][1] + 
     ". E é do tipo " + pokemon[0][2] 
 )
 console.log(
 	pokemon[1][0] + " Cadastrado com sucesso. " + 
     " Seu HP após a ultima luta é: " + pokemon [0][3] + 
-    ". Está no nivél " + pokemon[0][1] + 
+    ". Está no " + pokemon[0][1] + 
     ". E é do tipo " + pokemon[2][2] 
 )
 console.log(
 	pokemon[2][0] + " Cadastrado com sucesso. " + 
     " Seu HP após a ultima luta é: " + pokemon [2][3] + 
-    ". Está no nivél " + pokemon[1][1] + 
+    ". Está no " + pokemon[1][1] + 
     ". E é do tipo " + pokemon[3][2] 
 )
