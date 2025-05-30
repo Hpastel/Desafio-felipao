569ab35c379c4d19456b1435cf018a6c4f948f44
Hpastel <hermesalvesporto@gmail.com>
2025-05-30 15:06:35 +0100

    Expansão do codigo classificador de nível herói

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 let heroi = ["Superman", "Batman", "Mulher Maravilha", "Flash", "Aquaman", "Lanterna Verde", "Ciborgue", "Arqueiro Verde"];
 const nivel = [1000, 1001, 2001, 5001, 7001, 8001, 9001, 10001];
+  
 
-for (let i = 0; i < nivel.length; i++) { 
-    console.log("O Herói de nome " + heroi[7] + " está no nível de " + nivel[0]);
+console.log("O Herói de nome " + heroi[7] + " está no nível de " + nivel[0]);
+console.log(heroi.length + " Heróis foram cadastrados com sucesso.");
 
+let nivelHeroi = false
+let heroiEscolhido = heroi;
+
+if (nivelHeroi === "") {
+    console.log("Escolha um nivel " + nivel[console.nivel] + " e conheça o heroí");
+
+} else (nivelHeroi === null || nivelHeroi === undefined); {
+    return "Nenhum herói escolhido.";
 }
 
+
+// for (let i = 0; i < nivel.length; i++) { 
+// }
