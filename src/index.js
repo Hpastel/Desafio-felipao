919cd3c8c241290a699c9c2b6667aada6eccebf5
hermesAlves <hermesalvesporto@gmail.com>
2025-06-02 17:57:48 +0100

    correções codigo

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
 let heroi = ["Superman", "Batman", "Mulher Maravilha", "Flash", "Aquaman", "Lanterna Verde", "Ciborgue", "Arqueiro Verde"];
 const nivel = [1000, 1001, 2001, 5001, 7001, 8001, 9001, 10001];
   
+for (let i = 0; contador < heroi.length; i++) {
+    console.log("O herói " + heroi[i] + " está no nível " + nivel [i]);
+}
 
-// console.log("O Herói de nome " + heroi[7] + " está no nível de " + nivel[0]);
-// console.log(heroi.length + " Heróis foram cadastrados com sucesso.");
-
-let nivelHeroi = heroi + nivel;
-let heroiEscolhido = heroi === "";
-
-console.log(nivelHeroi[1]);
-
-
-if (nivelHeroi === "false") {
-    console.log("Escolha um nivel " + nivel[console.nivel] + " e conheça o heroí");
-
-} else (nivelHeroi === null || nivelHeroi === undefined); {
-    return "Nenhum herói escolhido.";
+let indiceEscolhido = 2;
+if (heroi[heroiEscolhido]) {
+    console.log("O Heroi de nome " + heroi[indiceEscolhido] + " está no nível " + nivel[indiceEscolhido]);
+} else  {
+    console.log("Nenhum herói escolhido.");
 }
 
 
-//  for (let contador = 1; contador < 1000; contador++) {
-//      console.log("O herói " + heroi[contador] + " está no nível " + nivel [0]);
-//  }
 
 
 
