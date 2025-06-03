d8cdc0efd671d41be2e75de1732afcd5704ba7b9
hermesAlves <hermesalvesporto@gmail.com>
2025-06-03 13:08:16 +0100

    Atualizações

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 let heroi = ["Superman", "Batman", "Mulher Maravilha", "Flash", "Aquaman", "Lanterna Verde", "Ciborgue", "Arqueiro Verde"];
-const nivel = [1000, 1001, 2001, 5001, 7001, 8001, 9001, 10001];
-  
+const experiencia = [1000, 1001, 2001, 5001, 7001, 8001, 9001, 10001];
+let nivel = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
+
 for (let i = 0; i < heroi.length; i++) {
-    console.log("O herói " + heroi[i] + " está no nível " + nivel [i]);
+    console.log("O herói " + heroi[i] + " está com  " + experiencia [i] + " de experiência e no nível " + nivel[i]);
 }
 
-let indiceEscolhido = 2;
-if (heroi[heroiEscolhido]) {
-    console.log("O Heroi de nome " + heroi[indiceEscolhido] + " está no nível " + nivel[indiceEscolhido]);
-} else  {
-    console.log("Nenhum herói escolhido.");
+heroi = "Batman";
+switch (heroi) {
+    case "Superman":
+    console.log("Use kryptonita para derrotar " + heroi + "!");
+    break;
+    
+    case "Batman":
+    console.log("Use o medo para derrotar " + heroi + "!");
+    break;
+
 }
 
 
@@ -18,8 +24,10 @@ if (heroi[heroiEscolhido]) {
 
 
 
-
-
-
+// if (heroi[indiceEscolhido]) {
+//     console.log("O Heroi de nome " + heroi[indiceEscolhido] + " está no nível " + nivel[indiceEscolhido]);
+// } else  {
+//     console.log("Nenhum herói escolhido.");
+// }
 
 
